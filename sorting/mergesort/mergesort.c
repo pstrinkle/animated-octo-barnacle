53d0944e97e9e3fb3e7beb1bc189fed9dd45bc80
Patrick Trinkle <patrick@activatr.com>
2016-08-26 16:19:25 -0400

    added some unit tests with assert instead of pulling cUnit into my requirements list.

--- sorting/mergesort/mergesort.c
+++ sorting/mergesort/mergesort.c
@@ -1,6 +1,8 @@
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define NUM_ELEMENTS(X) (sizeof(X)/sizeof(*X))
 
@@ -22,6 +24,11 @@ dumplist(int *input, int count)
 static int *
 _mergesort(int *input, int count)
 {
+    /* will not happen during recursion. */
+    if (input == NULL || count == 0) {
+        return NULL;
+    }
+
     if (count < 2) {
         /* XXX: return the array we received */
         int *t = malloc(sizeof(int));
@@ -91,6 +98,45 @@ int main(void)
     dumplist(sorted, NUM_ELEMENTS(unsorted));
 
     free(sorted);
+
+    /* some tests; better with cunit. */
+
+    /* empty list. */
+    {
+        int test[] = {};
+        int *result = _mergesort(test, NUM_ELEMENTS(test));
+        assert(result == NULL);
+    }
+
+    /* one item list, no sorting required. */
+    {
+        int test[] = {0};
+        int verify[] = {0};
+        int *result = _mergesort(test, NUM_ELEMENTS(test));
+        int res = memcmp(verify, result, sizeof(int) * NUM_ELEMENTS(test));
+        assert(0 == res);
+        free(result);
+    }
+
+    /* two items. */
+    {
+        int test[] = {1, 0};
+        int verify[] = {0, 1};
+        int *result = _mergesort(test, NUM_ELEMENTS(test));
+        int res = memcmp(verify, result, sizeof(int) * NUM_ELEMENTS(test));
+        assert(0 == res);
+        free(result);
+    }
+
+    /* n items. */
+    {
+        int test[] = {1, 0, 5, 7, 2, 9, 100, 14, 23, 3, 99};
+        int verify[] = {0, 1, 2, 3, 5, 7, 9, 14, 23, 99, 100};
+        int *result = _mergesort(test, NUM_ELEMENTS(test));
+        int res = memcmp(verify, result, sizeof(int) * NUM_ELEMENTS(test));
+        assert(0 == res);
+        free(result);
+    }
 
     return 0;
 }
