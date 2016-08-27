47de89711044ef2938bdfa29b511bc39ab2aa378
Patrick Trinkle <patrick@activatr.com>
2016-08-27 11:13:51 -0400

    started b-tree implementation
    Also loads of Makefile tweaks because newlines.
    
    So this b-tree implementation is both far from perfect and far from
    complete, but it’s a step in the right direction.  It handles splitting
    the root correctly (it appears to from testing) and it appears to
    support splitting a single normal block, and then subsequently
    splitting root.  I haven’t tried splitting multiple blocks in a row
    where one wasn’t root.  I also haven’t started delete at all!  My plan,
    today: get insert done.  Tomorrow: delete.

---
+++ trees/btree/btree.c
@@ -0,0 +1,470 @@
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_ELEMENTS(X) (sizeof(X)/sizeof(*X))
+
+/*
+ * I'm just going to implement this to store up to three keys per block.
+ */
+#define NUM_KEYS 3
+
+struct block;
+
+typedef struct key {
+    struct block *ptr;
+    int key;
+} key_t;
+
+typedef struct block {
+    int id; /* for humans. */
+    int used;
+    struct block *parent;
+    key_t keys[NUM_KEYS+1]; /* so that there's a trailing pointer. */
+} block_t;
+
+static block_t *root = NULL;
+static int counter = 0;
+
+static block_t *newBlock(void)
+{
+    block_t *b = malloc(sizeof(block_t));
+    memset(b, 0x00, sizeof(block_t));
+    b->id = counter++;
+
+    return b;
+}
+
+static void blockInsert(block_t *insert, key_t *promoted, block_t *newblk);
+static void blockSplit(block_t *blk);
+static void rootSplit(block_t *root);
+static void insert(block_t *root, int value);
+static void blockPrint(block_t *blk);
+static void depthFirstPrint(block_t *blk);
+
+/**
+ * @return 0 if not found
+ * @return 1 if found.
+ */
+static int search(block_t *blk, int value)
+{
+    block_t *where;
+    key_t *curr;
+    int i;
+    int found = 0;
+
+    where = blk;
+    while (where && !found) {
+        for (i = 0; i < where->used; i++) {
+            curr = &(where->keys[i]);
+            /* If it's less, we try going down the pointer. */
+            if (value < curr->key) {
+                if (curr->ptr) {
+                    where = curr->ptr;
+                    break;
+                } else {
+                    where = NULL;
+                    break;
+                }
+            } else if (value == curr->key) {
+                found = 1;
+                break;
+            } else {
+                /* moving on? */
+
+                if (i == (where->used-1)) {
+                    /* We're greater than the last one. */
+                    curr += 1;
+                    if (curr->ptr) {
+                        where = curr->ptr;
+                        break;
+                    } else {
+                        /*
+                         * If there's nothing beyond it, then it isn't there.
+                         */
+                        where = NULL;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return found;
+}
+
+static void blockInsert(block_t *insert, key_t *promoted, block_t *newblk)
+{
+//    int i;
+
+    printf("blockInsert to blk: %d, key: %d, newblk: %d\n",
+            insert->id,
+            promoted->key,
+            newblk->id);
+
+    insert->keys[insert->used].ptr = promoted->ptr;
+    insert->keys[insert->used].key = promoted->key; /* i think this is good.. */
+
+    insert->keys[insert->used+1].ptr = newblk;
+    insert->used++;
+
+    if (NUM_KEYS == insert->used) {
+        printf("need to split %d\n", insert->id);
+
+        if (NULL == insert->parent) {
+            printf("need to split root variant\n");
+            return rootSplit(insert);
+        } else {
+            printf("need to call normal block split\n");
+        }
+    }
+
+
+
+//    for (i = 0; i < insert->used; i++) {
+//        /* if it goes in the middle.... XXX: need to re-visit. */
+//    }
+
+    /*
+     * This could need to be split; if so, I believe the promoted item changes.
+     */
+
+    return;
+}
+
+static void blockSplit(block_t *blk)
+{
+    /* We've received blk, which is full, and we need to split it. */
+
+    /*
+     * XXX: This code is for splitting a leaf at present but I can likely make
+     * it more generic by some more bookkeeping. :D
+     */
+
+    int middleIndex = NUM_KEYS / 2;
+    key_t *middle = &(blk->keys[middleIndex]); /* this guy will be promoted. */
+
+    /* can make generic. */
+    key_t promote;
+    promote.key = middle->key;
+    promote.ptr = blk; /* also, by promoting up, we don't need to reset the pointer. */
+    /* XXX: if this already has a pointer set, we need to know and treat appropriately */
+
+    /* these will be copied into a new block. */
+    key_t *rightStart = middle + 1;
+    key_t *end = &(blk->keys[blk->used]);
+    size_t rightSize = (size_t)end - (size_t)rightStart;
+
+    block_t *newRight = newBlock();
+    key_t *newRightStart = &(newRight->keys[0]);
+
+    memcpy(newRightStart, rightStart, rightSize);
+    newRight->used = rightSize / sizeof(key_t);;
+    newRight->parent = blk->parent;
+
+    memset(middle, 0x00, rightSize + sizeof(key_t)); /* clear them off. */
+    blk->used = middleIndex; /* used is index+1 */
+
+    return blockInsert(blk->parent, &promote, newRight);
+}
+
+/*
+ * Splitting the root block is presently done as a special edge case, this will
+ * need to be upgraded (likely) down the road to better handle repeatedly
+ * splits that may end up here, such that pointers already present aren't lost.
+ */
+static void rootSplit(block_t *root)
+{
+    int i;
+    /* find middle key in block. */
+    int middleIndex = NUM_KEYS / 2;
+
+    printf("rootSplit...\n");
+    blockPrint(root);
+
+    /* pointer to the middle key, the one that will remain. */
+    key_t *middle = &(root->keys[middleIndex]);
+
+    /* start at the beginning of the keys. */
+    key_t *leftStart = &(root->keys[0]);
+    key_t *leftEnd = middle;
+    size_t leftSize = (size_t)leftEnd - (size_t)leftStart;
+
+    /* starts immediately following the middle. */
+    key_t *rightStart = middle + 1;
+    key_t *end = &(root->keys[root->used+1]); /* to include last ptr regardless */
+    size_t rightSize = (size_t)end - (size_t)rightStart;
+
+    block_t *newLeft = newBlock();
+    block_t *newRight = newBlock();
+
+    key_t *newLeftStart = &(newLeft->keys[0]);
+    key_t *newRightStart = &(newRight->keys[0]);
+
+    /*
+     * int specific, but could be generalized and stored on the stack
+     * (or heap).
+     */
+    int middleValue = middle->key;
+
+    int leftUsed = leftSize / sizeof(key_t);
+    /* rightUsed-1, because end includes the next entry. */
+    int rightUsed = (rightSize / sizeof(key_t)) - 1;
+
+    /* Set up new left. */
+    memcpy(newLeftStart, leftStart, leftSize);
+    newLeft->used = leftUsed;
+    newLeft->parent = root;
+    if (middle->ptr) {
+        newLeft->keys[newLeft->used].ptr = middle->ptr;
+    }
+
+    /* bookkeeping. */
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (newLeft->keys[i].ptr) {
+            newLeft->keys[i].ptr->parent = newLeft;
+        }
+    }
+
+    /* Set up new right. */
+    memcpy(newRightStart, rightStart, rightSize);
+    newRight->used = rightUsed;
+    newRight->parent = root;
+
+    /* bookkeeping. */
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (newRight->keys[i].ptr) {
+            newRight->keys[i].ptr->parent = newRight;
+        }
+    }
+
+    /* Rebuild root block. */
+    memset(root, 0x00, sizeof(block_t));
+    root->used = 1;
+    root->keys[0].key = middleValue;
+    root->keys[0].ptr = newLeft;
+    root->keys[1].ptr = newRight;
+
+    return;
+}
+
+static void insert(block_t *root, int value)
+{
+    block_t *where;
+    key_t *curr;
+    int i;
+    int found = 0;
+
+    printf("\nentered insert: %d\n", value);
+
+    /* first dude ever; could be called if all deleted. */
+    if (0 == root->used) {
+        root->used++;
+        root->keys[0].key = value;
+
+        printf("placed 0th root entry\n");
+        return;
+    }
+
+    /* could do this recursively, but meh. */
+    where = root;
+    while (!found) {
+        for (i = 0; i < where->used; i++) {
+            curr = &(where->keys[i]);
+
+            /* If the value is less, we follow the link. */
+            if (value < curr->key) {
+                if (curr->ptr) {
+                    where = curr->ptr;
+                } else {
+                    found = 1; /* we're at the lowest block. */
+                }
+                break;
+            } else {
+                /*
+                 * We're greater than the key; check if we're after the last
+                 * one.
+                 */
+                if (i == (where->used-1)) {
+                    /* we're at the last one, and we're greater, use pointer
+                     * to the right.
+                     */
+                    curr += 1;
+                    if (curr->ptr) {
+                        where = curr->ptr;
+                    } else {
+                        found = 1;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("found block: %d\n", where->id);
+
+    /*
+     * let's check this block to see if it goes there.
+     *
+     * we know that all the slots aren't in use yet or it would already be
+     * split, so we can guarantee placement, then split.
+     */
+
+    /*
+     * Really don't need to find this again; except later i may split up
+     * finding the block and finding where it goes.
+     */
+    for (i = 0; i < where->used; i++) {
+        curr = &(where->keys[i]);
+
+        if (value < curr->key) {
+            /* it goes here. */
+            /* we need to shift everything to the right by one. */
+
+            /* point immediately after the last memory slot. */
+            key_t *end = &(where->keys[where->used]);
+            size_t len = (size_t)end - (size_t)curr;
+
+            (void)memmove(curr + 1, curr, len);
+            curr->key = value;
+            where->used++;
+            break;
+        } else {
+            /* we're greater */
+            if (i == (where->used-1)) {
+                /* we're at the last slot in use; so we can just set after. */
+                where->keys[where->used].key = value;
+                where->used++;
+                break;
+            }
+        }
+    }
+
+    /* ok, now do we need to split where? */
+    if (NUM_KEYS == where->used) {
+        printf("need to split\n");
+
+        /* root split is different. */
+        if (root == where) {
+            return rootSplit(root);
+        } else {
+            /*
+             * Blocks know who their parents are, because that tiny amount
+             * of bookkeeping simplifies the implementation.  without it, I'd
+             * have to do some tracking and unwinding.
+             */
+            return blockSplit(where);
+        }
+    }
+
+    return;
+}
+
+static void blockPrint(block_t *blk)
+{
+    int i;
+    block_t *b;
+
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (blk->keys[i].ptr) {
+            b = blk->keys[i].ptr;
+            printf("ptr->%d ", b->id);
+        } else {
+            printf("-- ");
+        }
+        if (i < blk->used) {
+            printf("val: %d | ", blk->keys[i].key);
+        } else {
+            printf("| ");
+        }
+    }
+    printf("\n");
+}
+
+static void depthFirstPrint(block_t *blk)
+{
+    int i;
+    block_t *b;
+
+    if (blk->parent == NULL) {
+        printf("root: %d, used: %d | ", blk->id, blk->used);
+    } else {
+        printf("blk: %d, par: %d, used: %d | ", blk->id, blk->parent->id, blk->used);
+    }
+
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (blk->keys[i].ptr) {
+            b = blk->keys[i].ptr;
+            printf("ptr->%d ", b->id);
+        } else {
+            printf("-- ");
+        }
+        if (i < blk->used) {
+            printf("val: %d | ", blk->keys[i].key);
+        } else {
+            printf("| ");
+        }
+    }
+    printf("\n");
+
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (blk->keys[i].ptr) {
+            depthFirstPrint(blk->keys[i].ptr);
+        }
+    }
+}
+
+static void depthFirstFree(block_t *blk)
+{
+    int i;
+
+    for (i = 0; i <= NUM_KEYS; i++) {
+        if (blk->keys[i].ptr) {
+            depthFirstFree(blk->keys[i].ptr);
+        }
+    }
+
+    free(blk);
+}
+
+int main(void)
+{
+    int i;
+    //int input[] = {1, 2, 3, 4, 5};
+
+#define FIRSTVALUE 1
+#define INSERTCOUNT 7
+#define POSTLASTVALUE (FIRSTVALUE + INSERTCOUNT)
+
+    if (NULL == root) {
+        root = newBlock();
+    }
+
+//    for (i = 0; i < NUM_ELEMENTS(input); i++) {
+    for (i = FIRSTVALUE; i < POSTLASTVALUE; i++) {
+        //insert(root, input[i]);
+        insert(root, i);
+        depthFirstPrint(root);
+    }
+
+#if 0
+    /* verify we can find all nodes entered in. */
+    {
+        int f;
+        for (i = FIRSTVALUE; i < POSTLASTVALUE; i++) {
+            //f = search(root, input[i]);
+            f = search(root, i);
+            assert(f == 1);
+        }
+    }
+#endif
+
+    if (root) {
+        depthFirstPrint(root);
+        depthFirstFree(root);
+    }
+
+    return 0;
+}
