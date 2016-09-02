60a481bbe2350baba09f733dfe0780d6b27a261b
Patrick Trinkle <patrick@activatr.com>
2016-09-02 18:47:44 -0400

    started, no real progress

--- trees/redblacktree/redblacktree.py
+++ trees/redblacktree/redblacktree.py
@@ -4,20 +4,56 @@ from json import dumps
 RED = 1
 BLACK = 0
 
+def color(value):
+    if value == RED:
+        return "red"
+    else:
+        return "black"
+
 class Node(object):
     """Red-black Tree nodes."""
 
     def __init__(self, value):
         self.value = value
         self.color = RED
+
         # node less than
         self.left = None
         # node greater than
         self.right = None
+
         # parent pointer for convenience (back-link)
         self.parent = None
 
         return
+
+def insert(root, node):
+    """Insert a node."""
+
+    return
+
+def depthFirstPrint(node):
+    if self.parent is None:
+        # root
+        print "root: ",
+    else:
+        print "node: ",
+    
+    left = "nil"
+    right = "nil"
+
+    if self.left is not None:
+        left = self.left.value
+    if self.right is not None:
+        right = self.right.value
+
+    print "%d color: %s, left: %d, right: %d" \
+            % (self.value, color(self.color), left, right)
+
+    if self.left:
+        depthFirstPrint(self.left)
+    if self.right:
+        depthFirstPrint(self.right)
 
 def main():
 
